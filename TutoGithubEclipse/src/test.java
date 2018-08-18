366df01a0738f17ecbe507d890e81780dd76aee4
taktouniat <taktouniat@yahoo.fr>
2018-08-18 20:25:43 +0200

    troisieme changement

--- TutoGithubEclipse/src/test.java
+++ TutoGithubEclipse/src/test.java
@@ -3,7 +3,8 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println(" 1 - Tuto Git and eclipse");
-		System.out.println("2 - Tuto Git and eclipse");
+		System.out.println(" 2 - Tuto Git and eclipse");
+		System.out.println(" 3 - Tuto Git and eclipse");
 
 	}
 
